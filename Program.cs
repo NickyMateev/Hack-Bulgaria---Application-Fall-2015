7f11bf31aeaf15686c19da25307ee978174d6b90
NickyMateev <bgdunkers@gmail.com>
2015-10-30 23:32:57 +0200

    1-Points
    You are on a infinite 2D coordinate system.
    Your job is to follow the directions of the arrow and output the final
    position that you have arrived, starting from your own.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Points
+{
+    class Program
+    {
+        static void Main()
+        {
+            // starting position
+            int current_x = 0;
+            int current_y = 0;
+
+            // directions
+            string directions = ">>><<<~>>>~^^^";
+
+            bool inversed = false;
+
+            for (int i = 0; i < directions.Length; i++)
+            {
+                if (inversed)
+                {
+                    switch (directions[i])
+                    {
+                        case '>':
+                            current_x--;
+                            break;
+                        case '<':
+                            current_x++;
+                            break;
+                        case 'v':
+                            current_y--;
+                            break;
+                        case '^':
+                            current_y++;
+                            break;
+                        case '~':
+                            inversed = !inversed;
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (directions[i])
+                    {
+                        case '>':
+                            current_x++;
+                            break;
+                        case '<':
+                            current_x--;
+                            break;
+                        case 'v':
+                            current_y++;
+                            break;
+                        case '^':
+                            current_y--;
+                            break;
+                        case '~':
+                            inversed = !inversed;
+                            break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Final position: ({0}, {1})", current_x, current_y);
+
+        }
+    }
+}
