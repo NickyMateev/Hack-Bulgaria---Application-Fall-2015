a870fdb3dd32979677afac0c5861c4b08b301e0e
NickyMateev <bgdunkers@gmail.com>
2015-10-30 23:45:27 +0200

    3-Dependencies-Resolving
    You are implementing a package manager for the new hot language -
    Java.JS. This is a Java language that compiles to JavaScript.
    There are 3rd party packages that can be installed. All packages are
    described in all_packages.json.
    Your job is to implement a simple command-line tool that reads
    dependencies.json file and outputs what else should be installed in
    order for everything to work.

---
+++ DependenciesResolving.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Depedencies_Resolving
+{
+    class DependenciesResolving
+    {
+        // reading and saving the information from the .json files
+        // modify file location(the path) for personal use
+        public static JObject dependencies = JObject.Parse(File.ReadAllText(@"C:\Users\Nick\Desktop\project\dependencies.json"));
+        public static JObject all_packages = JObject.Parse(File.ReadAllText(@"C:\Users\Nick\Desktop\project\all_packages.json"));
+
+        // recursion function
+        public static void Install(string package)
+        {
+            string path = "C:\\Users\\Nick\\Desktop\\project\\installed_modules\\" + package;
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine("{0} is already installed.", package);
+            }
+            else
+            {
+                foreach (var current_package in all_packages)
+                {
+                    if (current_package.Key == package)
+                    {
+                        Console.WriteLine("Installing {0}.", package);
+                        Directory.CreateDirectory(path);
+
+                        int numOfDependentPackages = current_package.Value.Count(); // number of dependent packages
+                        if (numOfDependentPackages != 0)
+                        {
+                            Console.Write("In order to install {0}, we need ", package);
+                            for (int i = 0; i < numOfDependentPackages; i++)
+                            {
+                                if (i != numOfDependentPackages - 1)    // if it's not the last iteration
+                                {
+                                    Console.Write("{0} ", current_package.Value[i]);
+                                }
+                                else if (i != 0)
+                                {
+                                    Console.WriteLine("and {0}.", current_package.Value[i]);
+                                }
+                                else  // this is the case where there is only one dependent package to be installed
+                                {
+                                    Console.WriteLine("{0}.", current_package.Value[i]);
+                                }
+                            }
+
+                            // the actual recursion function:
+                            for (int i = 0; i < numOfDependentPackages; i++)
+                            {
+                                Install(current_package.Value[i].ToString());
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Main()
+        {
+            // from dependencies.json
+            string dependencyPackage = "";
+
+            foreach (var item in dependencies)
+            {
+                if (item.Key == "dependencies")
+                {
+                    dependencyPackage = item.Value.First.ToString();
+                }
+            }
+
+            // installing the packages (using recursion function)
+            Install(dependencyPackage);
+            Console.WriteLine("All done.");
+        }
+    }
+}
